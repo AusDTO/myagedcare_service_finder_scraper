41fab477835019d49dc13ec10beb165761742183
Matthew Landauer <mlandauer@gmail.com>
2016-06-21 13:12:06 +1000

    Find services

--- scraper.rb
+++ scraper.rb
@@ -25,9 +25,48 @@ def get_service_types
   result["retrieveServiceCatalogueResponse"]["retrieveServiceCatalogueOutput"]["services"]["service"].map{|t| t["serviceType"]}
 end
 
+def extract_leaf_nodes(h)
+  # Pick out the leaf nodes and name attributes based on the leaf nodes
+  result = {}
+  #item.
+  h.each do |k, v|
+    if v.kind_of?(Hash)
+      result.merge!(extract_leaf_nodes(v))
+    else
+      result[k] = v
+    end
+  end
+  result
+end
+
 # agent = Mechanize.new
 #
 # records = get_suburbs_chunk(agent)[:records]
 # p records
 
-p get_service_types
+# p get_service_types
+
+def find_services(serviceType, suburb, state, postcode)
+  request_body = {
+    "helpAtHomeFinderRequest" => {
+      "helpAtHomeFinderInput" => {
+        "serviceType" => serviceType,
+        "clientLocationSearch" => {
+          "localitySearch" => {
+            "suburb" => suburb,
+            "state" => state,
+            "postcode" => postcode
+          }
+        }
+      }
+    }
+  }
+
+  data = JSON.parse(RestClient.post("https://servicefinder.myagedcare.gov.au/api/acg/v1/helpAtHomeFinder",
+    request_body.to_json,
+    content_type: 'application/json', x_api_key: '7ca4c25771c54ca283c682a185e72277'))
+  items = data["helpAtHomeFinderResponse"]["helpAtHomeFinderOutput"]["helpAtHomeServices"]["helpAtHomeService"]
+  items.map{|i| extract_leaf_nodes(i)}
+end
+
+p find_services("Personal Care", "KATOOMBA", "NSW", "2780")
